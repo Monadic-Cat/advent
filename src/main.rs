14ab8d24b3742c1055252d0c34ad157618627c7c
Monadic Cat <greenmonadcat@gmail.com>
2020-12-05 10:38:43 -0600

    remove commented out input fetching code

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,6 @@ mod day_2;
 
 mod input {
     use ::std::io::Result;
-    // const BASE_URL: &str = "https://adventofcode.com/2020";
-    
-    // async fn fetch_input(client: &::reqwest::Client, day: u8) -> Result<Bytes> {
-    //     Ok(client.get(&format!("{}/day/{}/input", BASE_URL, day)).send().await?.bytes().await?)
-    // }
     pub fn fetch(day: u8) -> Result<Vec<u8>> {
         ::std::fs::read(format!("{}/input/day_{}", env!("CARGO_MANIFEST_DIR"), day))
     }
